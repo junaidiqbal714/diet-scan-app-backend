748833534df9959ca0da7a5f8bb5aab57b69a695
Arham-Rumi <arhamrumi007@gmail.com>
2024-04-21 17:33:04 +0500

    Added Calories module

--- handlers/calorie/list_calories.js
+++ handlers/calorie/list_calories.js
@@ -1,9 +1,15 @@
+const moment = require("moment");
 const { Calorie } = require("../../models/calorie");
 const { CATCH_BAD_REQUEST } = require("../../utils/utils");
 
 const list_calories = async (req, res) => {
   try {
-    const calories = await Calorie.find({ user_id: req.user._id }).lean();
+    const today = moment().format("YYYY-MM-DD");
+
+    const calories = await Calorie.find({
+      user_id: req.user._id,
+      date: new Date(today),
+    }).lean();
 
     res.status(200).json({
       code: 200,
